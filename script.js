5127b3b085e40806aee44b2fef4f55900ade32cd
am1553 <aryanmehta2016@gmail.com>
2021-11-03 15:57:59 +0000

    completed mobile version and doing tablet version now

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 const menuBtn = document.getElementById("menu-btn");
 const menuBtnImg = document.getElementById("menubtn-img");
 const navPanel = document.getElementById("nav-panel");
+const prevBtn = document.getElementById("previous");
+const nextBtn = document.getElementById("next");
 
 
-// EVENTLISTENER
+// MENU
 menuBtn.addEventListener("click", ()=> {
     navPanel.classList.toggle("nav-toggle");
     if(menuBtnImg.getAttribute('src') === "assets/mobile/icon-hamburger.svg") {
@@ -14,3 +16,32 @@ menuBtn.addEventListener("click", ()=> {
         menuBtnImg.setAttribute('src', "assets/mobile/icon-hamburger.svg");
     }
 })
+
+
+// PROJECT CAROUSEL
+
+let counter = 1;
+showSlides(counter);
+
+// next prev controls
+function plusSlide(n) {
+    showSlides(counter += n);
+}
+
+function showSlides(n) {
+    var i;
+    var project = document.getElementsByClassName("project");
+
+    if(n > project.length) {
+        counter = 1;
+    }
+    if(n < 1) {
+        counter = project.length;
+    }
+
+    for(i = 0; i < project.length; i++) {
+        project[i].style.display = "none";
+    }
+
+    project[counter - 1].style.display = "block";
+}
